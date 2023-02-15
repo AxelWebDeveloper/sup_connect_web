64467ae1bb23948386d360be7a6701aee45ccc6f
Oceane-LEGOFF <oceanekegoff@gmail.com>
2023-02-15 17:01:12 +0100

    tab note

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -149,24 +149,24 @@ const Dashboard = () => {
             <div class="text">Note</div>
             <table>
                 <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Points</th>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Note</th>
                 </tr>
                 <tr>
                     <td>Jill</td>
                     <td>Smith</td>
-                    <td>50</td>
+                    <td>10</td>
                 </tr>
                 <tr>
                     <td>Eve</td>
                     <td>Jackson</td>
-                    <td>94</td>
+                    <td>15</td>
                 </tr>
                 <tr>
                     <td>Adam</td>
                     <td>Johnson</td>
-                    <td>67</td>
+                    <td>19</td>
                 </tr>
             </table>
         </div>
