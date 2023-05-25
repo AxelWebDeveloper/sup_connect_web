6f58ea1e879e80983a82f5369d18bfbe30674949
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2023-05-25 14:13:53 +0200

    web-home-stat

--- src/views/Assiduite/Assiduite_po.jsx
+++ src/views/Assiduite/Assiduite_po.jsx
@@ -1,6 +1,5 @@
 import React, {useState, ChangeEvent, useEffect} from 'react';
 import styled from 'styled-components'
-import { useForm } from "react-hook-form";
 
 import Select from 'react-select';
 import AttendanceSwitch from "./AttendanceSwitch";
